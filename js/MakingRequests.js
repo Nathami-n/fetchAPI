efa3373efc598591a47b5a57dce1df084a31ed17
Nathan George <georgenathan010@gmail.com>
2024-01-07 22:48:45 +0300

    added: fetching data using promise chaining

--- js/MakingRequests.js
+++ js/MakingRequests.js
@@ -1,6 +1,15 @@
 // using promise chaining
 
-//importing the http module
-import http from 'http';
+//fetching data from a dummy api
+const apiUrl = "https://reqres.in/api/users";
 
-console.log(http);
+//making GET request using promise chaining
+//comment out this section to see the result
+// let response = fetch(apiUrl);
+// response.then(data => {
+//     return data.json()
+// })
+// .then(value => {
+//     let userData = value.data;
+//     console.log(userData);
+// })
